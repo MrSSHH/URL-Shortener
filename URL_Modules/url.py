79724f42a291dc97ae628baf9eb1460cb7c28dd3
Benjamin <44535859+MrSSHH@users.noreply.github.com>
2019-08-26 13:52:06 +0300

    Major fixes
    This update fixes the class fully.
    And some minor bugs.

--- URL_Modules/url.py
+++ URL_Modules/url.py
@@ -3,22 +3,35 @@ __github__  = "https://github.com/MrSSHH/"
 
 import random
 import string
-import sys
-import requests
 import os
 from errors import *
+import socket
+
+# This should be installed on client side
+
+
+try:
+
+    import pyperclip
+    import requests
+except ImportError:
+    print("Please download all modules from requirements.txt")
+
+
 
 class url:
+
     main_dir = os.path.join(os.path.dirname(__file__)) + "\database\database.data"
-    
+    main_ip = socket.gethostbyname(socket.gethostname())
     if not os.path.exists(main_dir):
-        os.mkdir(url.main_dir.replace("\database.data", "\\"))
+        os.mkdir(main_dir.replace("\database.data", "\\"))
         data = open(main_dir, "w+")
         data.close()
 
-    def __init__(self, link, database=main_dir):
+    def __init__(self, link, database=main_dir, host=main_ip):
         self.link = link
         self.database = database
+        self.host = host
 
     def __str__(self):
         return "<URL Object An Instance Of '{}'>".format(
@@ -43,7 +56,7 @@ class url:
             self.link = "http://" + self.link
 
         try:
-            response = requests.get(self.link)
+            requests.get(self.link)
         except Exception as e:
             raise WebsiteNotFound("Can't reach the website, please check the link.")
 
@@ -56,10 +69,10 @@ class url:
             except IndexError:
                 pass
             code = url.randomize()
-            full_link = "short.link/" + ''.join(code)
+            full_link = f"{self.host}/" + ''.join(code)
             link.write("{} - {}\n".format(self.link, full_link))
             return full_link
 
-        
-l = url("www.google.com")
-print(l.short())
+
+test = url(<your-URL>, host="10.0.0.8").short()
+pyperclip.copy(test)
